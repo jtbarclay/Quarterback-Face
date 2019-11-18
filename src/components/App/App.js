ca52c4794c630bee7fce4a6c21a7cbb4ce62dc07
Jon Barclay <my@nameisjon.com>
2019-11-18 13:00:10 -0600

    initial commit

---
+++ src/components/App/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class App extends Component {
+    render() {
+        return (
+            <div>
+                Hello world!
+            </div>
+        )
+    }
+}
+
+export default App
