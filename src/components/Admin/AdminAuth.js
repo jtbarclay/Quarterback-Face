1591096758d4152b0a2fad5bc0ca7384415bcaed
Jon Barclay <my@nameisjon.com>
2019-11-21 14:04:42 -0600

    admin route created

---
+++ src/components/Admin/AdminAuth.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+export class AdminAuth extends Component {
+    render() {
+        return (
+            <div>
+                ADMIN!!!
+            </div>
+        )
+    }
+}
+
+export default AdminAuth;
